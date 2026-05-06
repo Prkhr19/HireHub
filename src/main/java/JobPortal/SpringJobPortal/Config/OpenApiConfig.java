8f5741eb29b89b3526f9cc1e304f8cf89a10c70a
Prakhar Shrivastava <prkhr.work@gmail.com>
2026-05-06 11:25:27 +0530

    fix swagger server base url

--- src/main/java/JobPortal/SpringJobPortal/Config/OpenApiConfig.java
+++ src/main/java/JobPortal/SpringJobPortal/Config/OpenApiConfig.java
@@ -5,16 +5,24 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenApi(){
-        return new OpenAPI().info(new Info().title("Hire hub API")
+        Server server = new Server();
+        server.setUrl("/api/v1");
+        return new OpenAPI()
+                .servers(List.of(server))
+                .info(new Info().title("Hire hub API")
                 .version("1.0")
                 .description("Backend APIs for HireHub"))
+
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme().name("bearerAuth").type(SecurityScheme.Type.HTTP)
